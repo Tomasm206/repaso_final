aae520723f9eeddac6480b117ba5b185033a4c25
Tomas Marin <marintomas90@gmail.com>
2023-11-09 11:15:51 -0500

    v1.4

---
+++ src/com/agencia/dominio/exeptions/InmuebleSinLicenciaException.java
@@ -0,0 +1,4 @@
+package com.agencia.dominio.exeptions;
+
+public class InmuebleSinLicenciaException extends RuntimeException{
+}
---
+++ src/com/agencia/dominio/exeptions/RecientementeRenovadoException.java
@@ -0,0 +1,4 @@
+package com.agencia.dominio.exeptions;
+
+public class RecientementeRenovadoException extends RuntimeException{
+}
